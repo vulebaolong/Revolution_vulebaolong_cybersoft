987126dcb2609e8d6655e3b0e74373f54fcc4d27
unknown <vulebaolong@gmail.com>
2022-09-24 15:23:32 +0700

    done head 1

--- js/java.js
+++ js/java.js
@@ -1,36 +1,31 @@
 Click_header__nav()
 
 function Click_header__nav(params) {
+    var header = document.querySelector('header')
+
     var header__nav = document.querySelector('.header__nav')
-    var logo = document.querySelector('.logo')
-    // var logo_img2 = document.querySelector('.logo_img2')
-    var header__navTopup = document.querySelector('.header__navTopup')
     header__nav.onclick = (e) => {
-        showhide(logo,header__navTopup)
+        show(header,'logo__hide','logo__show')
     }
 
     var navTopup__control = document.querySelector('.navTopup__control')
     navTopup__control.onclick = () => {
-        showhide(logo,header__navTopup)
+        hide(header,'logo__hide','logo__show')
     }
 }
 
 
-function showhide(logo,header__navTopup) {
-    if ( logo.classList.contains('logo__hide') ) {
-        logo.classList.remove('logo__hide')
-        logo.classList.add('logo__show')
+function show(header,logo__hide,logo__show) {
+    if ( header.classList.contains(logo__hide) ) {
+        header.classList.remove(logo__hide)
+        header.classList.add(logo__show)
 
-    }else {
-        logo.classList.remove('logo__show')
-        logo.classList.add('logo__hide')       
     }
+}
 
-    if (header__navTopup.classList.contains('header__navTopupShow')) {
-        header__navTopup.classList.remove('header__navTopupShow')
-        header__navTopup.classList.add('header__navTopupHide')
-    }else {
-        header__navTopup.classList.remove('header__navTopupHide')
-        header__navTopup.classList.add('header__navTopupShow')
+function hide(header,logo__hide,logo__show) {
+    if (header.classList.contains(logo__show) ){
+        header.classList.remove(logo__show)
+        header.classList.add(logo__hide)       
     }
 }
